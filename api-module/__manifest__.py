50953df070331214c0f93f12dc01d941cf4aa356
gecm-odoo <gecm@odoo.com>
2022-07-06 10:52:05 -0500

    Changed manifest

--- api-module/__manifest__.py
+++ api-module/__manifest__.py
@@ -1,18 +1,24 @@
 {
-    'name': 'Odoo PDF',
-    'sumary': 'Bottling Experts: Custom Quotation and Invoice Qweb PDF Reports',
+    'name': 'Generic API Connector',
+    'summary': """Module to make an API connection using automated actions.""",
     'description': """
-        odoo PDF customize the PDF report of Quotation and Invoice
+        This module will add an option in the automated actions state menu, this option will let the user connect to an API.
+        There will be fields that the user needs to fill like authtoken and url for example and the model will convert those inputs into the params
+            that the API call will send.
+        
+        Developers: [gecm], [romf], [ayhp]
+        Task ID: 2856455
+        Link to project: https://www.odoo.com/web#id=2856455&menu_id=4720&cids=17&action=333&active_id=6182&model=project.task&view_type=form
     """,
     'author': 'Odoo PS',
-    'category': 'Sales',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
     'version': '15.0.1.0.0',
-    'depends': ['sale', 'web'],
-    'license': 'OPL-1',
-    'data': [
-        'views/view_base_automation_inherit.xml'
+    'depends': [
+        'sale',
     ],
-    'external_dependencies': {
-        'python': ['xmltodict'],
-    }
+    'data': [
+        'views/view_base_automation_inherit.xml',
+    ],
+    'license': 'OPL-1',
 }
--- api-module/models/__init__.py
+++ api-module/models/__init__.py
@@ -1,2 +1,1 @@
-#from . import base_automation
 from . import api_call
