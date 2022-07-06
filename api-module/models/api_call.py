214c9075c190115565f9e8f732728cdbd6f47b4c
gecm-odoo <gecm@odoo.com>
2022-07-06 16:48:41 -0500

    Added the functions xml2json and dic2xml

--- api-module/models/api_call.py
+++ api-module/models/api_call.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
+from xml.etree import cElementTree as ET
+import requests
+import json
+
 from odoo import models, fields, api
 from odoo.exceptions import UserError
-import requests
 
 
 class IrActionsServer(models.Model):
@@ -63,3 +67,53 @@ class IrActionsServer(models.Model):
             api_result = requests.post(self.url,  data=xml, headers=headers)
             raise UserError(api_result.json().get(
                 'recived data').get('note').get('to')[0])
+
+    def xml2dict(t):
+        d = {t.tag: {} if t.attrib else None}
+        children = list(t)
+        if children:
+            dd = defaultdict(list)
+            for dc in map(xml2dict, children):
+                for k, v in dc.items():
+                    dd[k].append(v)
+            d = {t.tag: {k: v[0] if len(v) == 1 else v
+                         for k, v in dd.items()}}
+        if t.attrib:
+            d[t.tag].update(('@' + k, v)
+                            for k, v in t.attrib.items())
+        if t.text:
+            text = t.text.strip()
+            if children or t.attrib:
+                if text:
+                    d[t.tag]['#text'] = text
+            else:
+                d[t.tag] = text
+        return d
+
+    def json2xml(json_obj, line_padding="", origin_obj=None):
+        result_list = list()
+
+        json_obj_type = type(json_obj)
+
+        if json_obj_type is list:
+            for sub_elem in json_obj:
+                result_list.append("%s<%s>" % (line_padding, origin_obj))
+                result_list.append(json2xml(sub_elem, "\t" + line_padding))
+                result_list.append("%s</%s>" % (line_padding, origin_obj))
+
+            return "\n".join(result_list)
+
+        if json_obj_type is dict:
+            for tag_name in json_obj:
+                sub_obj = json_obj[tag_name]
+                if type(sub_obj) is list:
+                    result_list.append(
+                        json2xml(sub_obj, line_padding, tag_name))
+                else:
+                    result_list.append("%s<%s>" % (line_padding, tag_name))
+                    result_list.append(json2xml(sub_obj, "\t" + line_padding))
+                    result_list.append("%s</%s>" % (line_padding, tag_name))
+
+            return "\n".join(result_list)
+
+        return "%s%s" % (line_padding, json_obj)
