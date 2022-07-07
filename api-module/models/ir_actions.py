ae190fe341a2ff8cdeb80f9b790301f6d3ca8481
gecm-odoo <gecm@odoo.com>
2022-07-07 12:57:17 -0500

    [BUG] Bug in header view

--- api-module/models/ir_actions.py
+++ api-module/models/ir_actions.py
@@ -24,21 +24,26 @@ class IrActionsServer(models.Model):
         string='Content Type',
         required=True,
     )
-    headers = fields.One2many(string='Headers', inverse_name='api_id' ,comodel_name='api.header')
-    payload = fields.Text() 
+    headers = fields.One2many(
+        string='Headers', inverse_name='server_id', comodel_name='api.header')
+
+    # headers = fields.One2many(
+    #     string='Headers', inverse_name='server_id', comodel_name='ir.server.object.lines')
+
+    payload = fields.Text()
 
     def create(self, vals_list):
         res = super(IrActionsServer, self).create(vals_list)
         for val in vals_list:
-            if not val.get('api_name'):
-                raise UserError('API must have a name')
-            if not val.get('url'):
-                raise UserError('API must have a url')
-            xml = val.get('payload')
-            headers = {'Content-Type': val.get('content_type')}
-            api_result = requests.post(
-                val.get('url'),  data=xml, headers=headers)
-            print(api_result.json())
+            if val.get('state') == 'api_call':
+                if not val.get('url'):
+                    raise UserError('API must have a url')
+                xml = val.get('payload')
+                headers = {'Content-Type': val.get('content_type')}
+                print(f"HEADERRRRRRRRRRRRRRSSSSSS {val.get('headers')}")
+                api_result = requests.post(
+                    val.get('url'),  data=xml, headers=headers)
+                print(api_result.json())
         return res
 
     def _run_action_api_call(self, eval_context=None):
@@ -50,7 +55,7 @@ class IrActionsServer(models.Model):
             api_result = requests.post(self.url,  data=xml, headers=headers)
             raise UserError(api_result.json())
 
-    def xml2dict(self,t):
+    def xml2dict(self, t):
         d = {t.tag: {} if t.attrib else None}
         children = list(t)
         if children:
@@ -72,7 +77,7 @@ class IrActionsServer(models.Model):
                 d[t.tag] = text
         return d
 
-    def json2xml(self,json_obj, line_padding="", origin_obj=None):
+    def json2xml(self, json_obj, line_padding="", origin_obj=None):
         result_list = list()
 
         json_obj_type = type(json_obj)
@@ -80,7 +85,8 @@ class IrActionsServer(models.Model):
         if json_obj_type is list:
             for sub_elem in json_obj:
                 result_list.append("%s<%s>" % (line_padding, origin_obj))
-                result_list.append(self.json2xml(sub_elem, "\t" + line_padding))
+                result_list.append(self.json2xml(
+                    sub_elem, "\t" + line_padding))
                 result_list.append("%s</%s>" % (line_padding, origin_obj))
 
             return "\n".join(result_list)
@@ -93,9 +99,19 @@ class IrActionsServer(models.Model):
                         self.json2xml(sub_obj, line_padding, tag_name))
                 else:
                     result_list.append("%s<%s>" % (line_padding, tag_name))
-                    result_list.append(self.json2xml(sub_obj, "\t" + line_padding))
+                    result_list.append(self.json2xml(
+                        sub_obj, "\t" + line_padding))
                     result_list.append("%s</%s>" % (line_padding, tag_name))
 
             return "\n".join(result_list)
 
         return "%s%s" % (line_padding, json_obj)
+
+
+# class ApiHeader(models.Model):
+#     _name = 'api.header'
+#     _description = 'API Header'
+
+#     key = fields.Char(string='Key')
+#     value = fields.Char(string='Value')
+#     api_id = fields.Many2one(string='API', comodel_name='ir.actions.server')
